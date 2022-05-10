df66b41f72bc80967cd2dce81cc0db15309935cb
Petre Stefan <59830261+hacchacc42@users.noreply.github.com>
2022-05-10 11:43:15 +0100

    Initial commit

---
+++ Source/TGP/TGPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TGPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TGP_API ATGPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
