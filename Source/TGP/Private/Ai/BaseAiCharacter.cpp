c6f21a9d944b5142ec740ad4cf7ab2d614b638fb
ghuk1 <george.humphreys@hotmail.co.uk>
2022-05-22 16:31:19 +0100

    Revert "Merge branch 'Stefan'"
    This reverts commit a2827b67304be19e7ee9f9cd3c22bf3c1703839a, reversing
    changes made to 13911c27408d8df0c81d8cca61d1c4c7798a5f34.

--- Source/TGP/Private/Ai/BaseAiCharacter.cpp
+++ Source/TGP/Private/Ai/BaseAiCharacter.cpp
@@ -1,9 +1,11 @@
 #include "Ai/BaseAiCharacter.h"
 #include "Ai/AiCharacterData.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/SphereComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "TGP/TGPGameModeBase.h"
+#include "TGP/FP_FirstPerson/FP_FirstPersonCharacter.h"
 
 ABaseAiCharacter::ABaseAiCharacter()
 {
@@ -13,6 +15,19 @@ ABaseAiCharacter::ABaseAiCharacter()
 	
 	HealthComponent = CreateDefaultSubobject<UHealthComponent>(TEXT("HealthComponent"));
 	AddOwnedComponent(HealthComponent);
+
+	RightHandCollider = CreateDefaultSubobject<USphereComponent>(TEXT("Right Hand Collider"));
+	RightHandCollider->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("RightArmCollider"));
+	RightHandCollider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	RightHandCollider->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
+	RightHandCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
+	
+	LeftArmCollider = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Left Arm Collider"));
+	LeftArmCollider->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("LeftArmCollider"));
+	LeftArmCollider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	LeftArmCollider->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
+	LeftArmCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
+	
 }
 
 void ABaseAiCharacter::BeginPlay()
@@ -22,6 +37,9 @@ void ABaseAiCharacter::BeginPlay()
 	HealthComponent->onComponentDead.AddDynamic(this, &ABaseAiCharacter::OnEnemyDied);
 	baseAiController = Cast<ABaseAIController>(GetController());
 	baseAiController->RunBT();
+
+	RightHandCollider->OnComponentBeginOverlap.AddDynamic(this, &ABaseAiCharacter::RHHitPlayer);
+	LeftArmCollider->OnComponentBeginOverlap.AddDynamic(this, &ABaseAiCharacter::LHHitPlayer);
 }
 
 void ABaseAiCharacter::Tick(float DeltaTime)
@@ -38,10 +56,32 @@ void ABaseAiCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 
 void ABaseAiCharacter::OnEnemyDied(AController* Causer)
 {
-	SetHidden(true);
+	// SetHidden(true);
+	//
+	// if (ATGPGameModeBase* GM = Cast<ATGPGameModeBase>(UGameplayStatics::GetGameMode(GetWorld())))
+	// 	GM->OnEnemyKilled(this);
 
-	if (ATGPGameModeBase* GM = Cast<ATGPGameModeBase>(UGameplayStatics::GetGameMode(GetWorld())))
-		GM->OnEnemyKilled(this);
+	UE_LOG(LogTemp, Warning, TEXT("Zombie Died"));
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+
+	GetCharacterMovement()->MaxWalkSpeed = 0.0f;
+
+	if(AnimInstance)
+	{
+		if(!GetMesh()->GetAnimInstance()->Montage_IsPlaying(DeathMontage))
+		{
+			int DeathAnim = FMath::RandRange(0, DeathMontage->CompositeSections.Num() - 1);
+			FName SectionName = DeathMontage->GetSectionName(DeathAnim);
+	
+			AnimInstance->Montage_Play(DeathMontage, 1.0f);
+			AnimInstance->Montage_JumpToSection(SectionName, DeathMontage);
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Animation"));
+	}
+	
 }
 
 void ABaseAiCharacter::SpawnEnemy(const FVector& RespawnPos)
@@ -53,6 +93,30 @@ void ABaseAiCharacter::SpawnEnemy(const FVector& RespawnPos)
 		GetMesh()->SetSkeletalMesh(EnemyStats->SkeletalMeshes[FMath::RandRange(0, EnemyStats->SkeletalMeshes.Num() - 1)]);
 		GetMesh()->SetAnimationMode(EAnimationMode::AnimationBlueprint);
 		GetMesh()->SetAnimInstanceClass(EnemyStats->EnemyAnimBP);
+
+		float SpeedGenerator[3] = {0.5f, 1.0f, 2.5f};
+		int SpeedIndex = FMath::RandRange(0, 2);
+	
+		switch (SpeedIndex)
+		{
+			case 0:
+				{
+					MovementType = EZombieMoveType::MoveState_SlowWalking;
+					break;
+				}
+			case 2:
+				{
+					MovementType = EZombieMoveType::MoveState_Running;
+					break;
+				}
+			default:
+				{
+					MovementType = EZombieMoveType::MoveState_NormalWalking;
+					break;
+				}			
+		}
+		
+		GetCharacterMovement()->MaxWalkSpeed = EnemyStats->DefaultWalkSpeed * SpeedGenerator[SpeedIndex];
 
 		HealthComponent->health = EnemyStats->DefaultHealth + (EnemyStats->DefaultHealth * FMath::RandRange(-EnemyStats->MaxDeviation, EnemyStats->MaxDeviation));
 		Damage = EnemyStats->DefaultDamage + (EnemyStats->DefaultDamage * FMath::RandRange(0.f, EnemyStats->MaxDeviation));
@@ -84,6 +148,98 @@ void ABaseAiCharacter::SetHidden(bool bEnemyHidden)
 
 void ABaseAiCharacter::Attack()
 {
-	GetMovementComponent()->StopMovementImmediately();
-	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Purple, "Attack");
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+
+	if(AnimInstance)
+	{
+		if(!GetMesh()->GetAnimInstance()->Montage_IsPlaying(AttackMontage) && !GetMesh()->GetAnimInstance()->Montage_IsPlaying(DeathMontage))
+		{
+			int AttackAnim = FMath::RandRange(0, AttackMontage->CompositeSections.Num() - 1);
+			FName SectionName = AttackMontage->GetSectionName(AttackAnim);
+
+			switch (AttackAnim)
+			{
+				case 0:
+				{
+					AnimInstance->Montage_Play(AttackMontage, 1.35f);
+					AnimInstance->Montage_JumpToSection(SectionName, AttackMontage);
+					break;
+				}
+				case 2:
+				{
+					AnimInstance->Montage_Play(AttackMontage, 1.15f);
+					AnimInstance->Montage_JumpToSection(SectionName, AttackMontage);
+					break;
+				}
+				case 3:
+				{
+					AnimInstance->Montage_Play(AttackMontage, 0.75f);
+					AnimInstance->Montage_JumpToSection(SectionName, AttackMontage);
+					break;
+				}
+				default:
+				{
+						AnimInstance->Montage_Play(AttackMontage, 1.0f);
+						AnimInstance->Montage_JumpToSection(SectionName, AttackMontage);
+					break;
+				}
+			}
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Animation"));
+	}
 }
+
+void ABaseAiCharacter::Die()
+{
+	SetHidden(true);
+
+	GetCharacterMovement()->MaxWalkSpeed = 100.0f;
+
+	if (ATGPGameModeBase* GM = Cast<ATGPGameModeBase>(UGameplayStatics::GetGameMode(GetWorld())))
+		GM->OnEnemyKilled(this);
+}
+
+void ABaseAiCharacter::LHHitPlayer(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(OtherActor->IsA(AFP_FirstPersonCharacter::StaticClass()))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Purple, TEXT("Damage Player"));
+		LeftColliderOff();
+	}
+}
+
+void ABaseAiCharacter::RHHitPlayer(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(OtherActor->IsA(AFP_FirstPersonCharacter::StaticClass()))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Purple, TEXT("Damage Player"));
+		RightColliderOff();
+	}
+}
+
+void ABaseAiCharacter::LeftColliderOn()
+{
+	LeftArmCollider->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+}
+
+void ABaseAiCharacter::LeftColliderOff()
+{
+	LeftArmCollider->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+}
+
+void ABaseAiCharacter::RightColliderOn()
+{
+	RightHandCollider->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+}
+
+void ABaseAiCharacter::RightColliderOff()
+{
+	RightHandCollider->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+}
+
+
--- Source/TGP/TGPGameModeBase.h
+++ Source/TGP/TGPGameModeBase.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #include "Item/BaseItem.h"
+
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "GameInstance/BaseGameInstance.h"
-#include "Misc/Region.h"
-
 #include "TGPGameModeBase.generated.h"
 
 class ABaseAiCharacter;
@@ -45,9 +44,6 @@ class TGP_API ATGPGameModeBase : public AGameModeBase
 	bool bPendingRoundRestart = true;
 	float SpawnerTimer = 0.f;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Regions", Meta = (AllowPrivateAccess = true)) TArray<ARegion*> regions;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Regions", Meta = (AllowPrivateAccess = true)) ARegion* currentRegion;
-
 	void BeginRound();
 	void EndRound();
 
@@ -59,8 +55,6 @@ class TGP_API ATGPGameModeBase : public AGameModeBase
 	bool CheckLineOfSight(const FVector& PawnLoc, const FVector& SpawnPoint) const;
 	bool IsLookingAtDir(const FVector& PawnDir, const FVector& DirToPoint) const;
 
-	void SetRegions();
-	
 	UFUNCTION(CallInEditor) void DEBUG_KILL_ENEMY();
 
 protected:
