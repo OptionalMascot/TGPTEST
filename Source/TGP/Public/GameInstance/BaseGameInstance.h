e57a191c8b6e014df1452285d1a2aee9ddf27590
mairis13 <mairis1353@gmail.com>
2022-05-22 22:53:02 +0100

    Sessions re-implementation. Disappeared for some reason.

--- Source/TGP/Public/GameInstance/BaseGameInstance.h
+++ Source/TGP/Public/GameInstance/BaseGameInstance.h
@@ -3,7 +3,7 @@
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "Item/ItemInfo.h"
-
+#include "Interfaces/OnlineSessionInterface.h"
 #include "BaseGameInstance.generated.h"
 
 class UItemInfo;
@@ -15,6 +15,27 @@ class TGP_API UBaseGameInstance : public UGameInstance
 
 	UPROPERTY() TArray<UItemInfo*> ItemInfos;
 
+	TSharedPtr<FOnlineSessionSettings> SessionSettings;
+
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+
+	FOnSessionInviteReceivedDelegate SessionInviteReceivedDelegate;
+	FDelegateHandle SessionInviteReceivedDelegateHandle;
+
+	FOnSessionUserInviteAcceptedDelegate SessionInviteAcceptedDelegate;
+	FDelegateHandle SessionInviteAcceptedDelegateHandle;
+	
+	FOnJoinSessionCompleteDelegate OnJoinSessionCompleteDelegate;
+	FDelegateHandle OnJoinSessionCompleteDelegateHandle;
+
+	void OnSessionInviteAccepted(const bool bSuccessful, int32 LocalPlayer, TSharedPtr<const FUniqueNetId> PersonInviting, const FOnlineSessionSearchResult& SessionToJoin);
+	void OnSessionInviteReceivedFromFriend(const FUniqueNetId& UserId, const FUniqueNetId& FromId, const FString& AppId, const FOnlineSessionSearchResult& Result);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	void OnCreateSessionCompleted(FName SessionName, bool bSuccessful);
+	
+	bool JoinFriendSession(TSharedPtr<const FUniqueNetId> LocalUserId, FName SessionName, const FOnlineSessionSearchResult& Result);
+
 	void OnLoadedInfos();
 	void LoadInfos();
 	
@@ -23,6 +44,12 @@ public:
 	
 	virtual void Init() override;
 	virtual void Shutdown() override;
+	
+	void CreateSession(int32 NumPublicConnections, bool bIsLanMatch);
+	bool SendSessionInvite(APlayerController *PlayerController, const FUniqueNetId& FriendId);
+	
+	FOnCreateSessionComplete OnCreateSessionCompleteEvent;
+	UTexture2D* GetFriendAvatar(TSharedRef<const FUniqueNetId> FriendId);
 
 	UItemInfo* FindInfoShortName(const FString& ItemShortName) const;
 	UItemInfo* FindInfoUniqueId(int UniqueId) const;
