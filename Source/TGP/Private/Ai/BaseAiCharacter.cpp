7129cdea69958f92b971285c14d3da8ec12cc633
George Humphreys <68791789+GeorgeSHumphreys@users.noreply.github.com>
2022-05-24 12:16:30 +0100

    Trying to fix ai char

--- Source/TGP/Private/Ai/BaseAiCharacter.cpp
+++ Source/TGP/Private/Ai/BaseAiCharacter.cpp
@@ -15,17 +15,17 @@ ABaseAiCharacter::ABaseAiCharacter()
 	HealthComponent = CreateDefaultSubobject<UHealthComponent>(TEXT("HealthComponent"));
 	AddOwnedComponent(HealthComponent);
 
-	RightHandCollider = CreateDefaultSubobject<USphereComponent>(TEXT("Right Hand Collider"));
-	RightHandCollider->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("RightArmCollider"));
+	//RightHandCollider = CreateDefaultSubobject<USphereComponent>(TEXT("Right Hand Collider"));
+	//RightHandCollider->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("RightArmCollider"));
 	//RightHandCollider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	RightHandCollider->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
-	RightHandCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
+	//RightHandCollider->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
+	//RightHandCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
 	
-	LeftArmCollider = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Left Arm Collider"));
-	LeftArmCollider->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("LeftArmCollider"));
-	LeftArmCollider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	LeftArmCollider->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
-	LeftArmCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
+	//LeftArmCollider = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Left Arm Collider"));
+	//LeftArmCollider->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("LeftArmCollider"));
+	//LeftArmCollider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	//LeftArmCollider->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
+	//LeftArmCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
 }
 
 void ABaseAiCharacter::BeginPlay()
