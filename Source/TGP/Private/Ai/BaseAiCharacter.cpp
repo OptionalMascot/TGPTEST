92ca0be72df385c1c44bb8f2ceae242477adb3a6
ghuk1 <george.humphreys@hotmail.co.uk>
2022-05-24 22:34:03 +0100

    Test

--- Source/TGP/Private/Ai/BaseAiCharacter.cpp
+++ Source/TGP/Private/Ai/BaseAiCharacter.cpp
@@ -16,44 +16,28 @@ ABaseAiCharacter::ABaseAiCharacter()
 	AddOwnedComponent(HealthComponent);
 
 	RightHandCollider = CreateDefaultSubobject<USphereComponent>(TEXT("Right Hand Collider"));
-	RightHandCollider->SetupAttachment(GetMesh());
-	
+	RightHandCollider->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("RightArmCollider"));
+	//RightHandCollider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	RightHandCollider->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
+	RightHandCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
 	
 	LeftArmCollider = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Left Arm Collider"));
-	LeftArmCollider->SetupAttachment(GetMesh());
+	LeftArmCollider->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("LeftArmCollider"));
+	LeftArmCollider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	LeftArmCollider->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
+	LeftArmCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
 }
 
 void ABaseAiCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	RightHandCollider->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("RightArmCollider"));
-	RightHandCollider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	RightHandCollider->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
-	RightHandCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
+	HealthComponent->onComponentDead.AddDynamic(this, &ABaseAiCharacter::OnEnemyDied);
+	baseAiController = Cast<ABaseAIController>(GetController());
+	baseAiController->RunBT();
 
-	LeftArmCollider->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("LeftArmCollider"));
-	LeftArmCollider->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	LeftArmCollider->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
-	LeftArmCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
-
-	//HealthComponent->onComponentDead.AddDynamic(this, &ABaseAiCharacter::OnEnemyDied);
-//
-	//if (GetWorld()->IsServer())
-	//{
-	//	baseAiController = Cast<ABaseAIController>(GetController());
-	//	baseAiController->RunBT();
-	//}
-	
-	if (HasAuthority())
-	{
-		HealthComponent->onComponentDead.AddDynamic(this, &ABaseAiCharacter::OnEnemyDied);
-		baseAiController = Cast<ABaseAIController>(GetController());
-		baseAiController->RunBT();
-
-		LeftArmCollider->OnComponentBeginOverlap.AddDynamic(this, &ABaseAiCharacter::LeftColliderHit);
-		RightHandCollider->OnComponentBeginOverlap.AddDynamic(this, &ABaseAiCharacter::RightColliderHit);
-	}
+	LeftArmCollider->OnComponentBeginOverlap.AddDynamic(this, &ABaseAiCharacter::LeftColliderHit);
+	RightHandCollider->OnComponentBeginOverlap.AddDynamic(this, &ABaseAiCharacter::RightColliderHit);
 }
 
 void ABaseAiCharacter::Tick(float DeltaTime)
@@ -109,17 +93,12 @@ void ABaseAiCharacter::SpawnEnemy(const FVector& RespawnPos)
 
 		HealthComponent->health = EnemyStats->DefaultHealth + (EnemyStats->DefaultHealth * FMath::RandRange(-EnemyStats->MaxDeviation, EnemyStats->MaxDeviation));
 		Damage = EnemyStats->DefaultDamage + (EnemyStats->DefaultDamage * FMath::RandRange(0.f, EnemyStats->MaxDeviation));
-
-		/*if (EnemyStats->AiControllerClass)
-		{
-			if (GetController()->StaticClass() != EnemyStats->AiControllerClass) // IF AI Controller is different replace with expected controller. (Used for swapping Ai to bosses/other enemy types)
-			{
-				AController* NewController = NewObject<AController>(EnemyStats->AiControllerClass);
-				NewController->Possess(this);
-			}
-		}*/	
 	}
 	SetMoveType();
+
+	if (ATGPGameModeBase* GM = Cast<ATGPGameModeBase>(UGameplayStatics::GetGameMode(GetWorld())))
+		baseAiController->SetObjective(GM->GetCurrentRegionObjective());
+	
 	SetHidden(false);
 }
 
--- Source/TGP/Public/GameInstance/BaseGameInstance.h
+++ Source/TGP/Public/GameInstance/BaseGameInstance.h
@@ -3,7 +3,7 @@
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "Item/ItemInfo.h"
-#include "Interfaces/OnlineSessionInterface.h"
+
 #include "BaseGameInstance.generated.h"
 
 class UItemInfo;
@@ -13,28 +13,7 @@ class TGP_API UBaseGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 
-	UPROPERTY() TArray<UItemInfo*> ItemInfos;
-
-	TSharedPtr<FOnlineSessionSettings> SessionSettings;
-
-	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
-	FDelegateHandle CreateSessionCompleteDelegateHandle;
-
-	FOnSessionInviteReceivedDelegate SessionInviteReceivedDelegate;
-	FDelegateHandle SessionInviteReceivedDelegateHandle;
-
-	FOnSessionUserInviteAcceptedDelegate SessionInviteAcceptedDelegate;
-	FDelegateHandle SessionInviteAcceptedDelegateHandle;
-	
-	FOnJoinSessionCompleteDelegate OnJoinSessionCompleteDelegate;
-	FDelegateHandle OnJoinSessionCompleteDelegateHandle;
-
-	void OnSessionInviteAccepted(const bool bSuccessful, int32 LocalPlayer, TSharedPtr<const FUniqueNetId> PersonInviting, const FOnlineSessionSearchResult& SessionToJoin);
-	void OnSessionInviteReceivedFromFriend(const FUniqueNetId& UserId, const FUniqueNetId& FromId, const FString& AppId, const FOnlineSessionSearchResult& Result);
-	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
-	void OnCreateSessionCompleted(FName SessionName, bool bSuccessful);
-	
-	bool JoinFriendSession(TSharedPtr<const FUniqueNetId> LocalUserId, FName SessionName, const FOnlineSessionSearchResult& Result);
+	TArray<UItemInfo*> ItemInfos;
 
 	void OnLoadedInfos();
 	void LoadInfos();
@@ -44,12 +23,12 @@ public:
 	
 	virtual void Init() override;
 	virtual void Shutdown() override;
-	
 	UFUNCTION(BlueprintCallable) void CreateSession(int32 NumPublicConnections, bool bIsLanMatch);
 	bool SendSessionInvite(APlayerController *PlayerController, const FUniqueNetId& FriendId);
 	
 	FOnCreateSessionComplete OnCreateSessionCompleteEvent;
 	UTexture2D* GetFriendAvatar(TSharedRef<const FUniqueNetId> FriendId);
+
 
 	UItemInfo* FindInfoShortName(const FString& ItemShortName) const;
 	UItemInfo* FindInfoUniqueId(int UniqueId) const;
